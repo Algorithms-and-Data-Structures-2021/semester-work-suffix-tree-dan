2d0e0525f16efd8a7d642d4d67ede2e572a93e1b
Danil Galeev <galoffdan@gmail.com>
2021-04-13 17:50:46 +0300

    Write struct SuffixTree

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -2,6 +2,8 @@
 
 // Заголовочный файл с объявлением структуры данных
 
+#include <unordered_map>
+#include <vector>
 namespace itis {
 
   // Tip 1: объявите здесь необходимые структуры, функции, константы и прочее
@@ -10,18 +12,23 @@ namespace itis {
   inline constexpr auto kStringConstant = "Hello, stranger!";
 
   // Пример: объявление структуры с полями и методами
-  struct MyStructure {
+  struct Node {
    public:
-    int size_{0};
-    int capacity_{0};
-    int* data_{nullptr};
-
+    Node* next_node;
+    std::unordered_map<char, int*> edges;
     // Tip 2: На начальном этапе разработки структуры данных можете определения методов задавать в
     // заголовочном файле, как только работа будет завершена, можно будет оставить здесь только объявления.
-
-    int size() const {
-      return size_;
-    }
   };
 
+  struct SuffixTree {
+    SuffixTree(std::string &);
+
+   private:
+    std::vector<Node> nodes;
+    void addSuffix(int const);
+    std::string str;
+    int* findEdge(char &, Node const&);
+   public:
+    void createTree(std::string &);
+  };
 }  // namespace itis
