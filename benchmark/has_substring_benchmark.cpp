19dcac53e13740cfbbc32f57c9b782c9c0ee38e9
Danil Galeev <galoffdan@gmail.com>
2021-04-25 23:10:04 +0300

    Write new benchmark for the method count all substrings

--- benchmark/has_substring_benchmark.cpp
+++ benchmark/has_substring_benchmark.cpp
@@ -9,35 +9,38 @@
 using namespace std;
 using namespace itis;
 int main(int /*argc*/, char ** /*argv*/) {
-  const string absolute_input_path = "/home/miguelius/CLionProjects/semester-work-suffix-tree-dan/dataset/data/has_substr";
-  const string absolute_output_path = "/home/miguelius/CLionProjects/semester-work-suffix-tree-dan/benchmark/results/has_substr";
-  ifstream test_str_file("/home/miguelius/CLionProjects/semester-work-suffix-tree-dan/dataset/data/has_substr/test-string.csv");
+  const string absolute_input_path =
+      "/home/miguelius/CLionProjects/semester-work-suffix-tree-dan/dataset/data/has_substr";
+  const string absolute_output_path =
+      "/home/miguelius/CLionProjects/semester-work-suffix-tree-dan/benchmark/results/has_substr";
+  ifstream test_str_file(
+      "/home/miguelius/CLionProjects/semester-work-suffix-tree-dan/dataset/data/has_substr/test-string.csv");
   string test_str;
-  if (test_str_file.is_open()){
+  if (test_str_file.is_open()) {
     getline(test_str_file, test_str);
-
   }
-  string *file_names = new string[12]{"100.csv",   "500.csv",    "1000.csv",   "5000.csv",   "10000.csv",  "25000.csv",
-                                      "50000.csv", "100000.csv", "250000.csv", "500000.csv", "750000.csv", "1000000.csv"};
+  string *file_names =
+      new string[12]{"100.csv",   "500.csv",    "1000.csv",   "5000.csv",   "10000.csv",  "25000.csv",
+                     "50000.csv", "100000.csv", "250000.csv", "500000.csv", "750000.csv", "1000000.csv"};
   SuffixTree suffixTree;
   suffixTree.createTree(test_str);
-  for(int i = 0; i < 12; i++){
+  for (int i = 0; i < 12; i++) {
     string file_name = file_names[i];
     ofstream output_file(absolute_output_path + "/" + file_name, ios::app);
-    for (int j = 1; j < 11; j++){
+    for (int j = 1; j < 11; j++) {
       output_file << to_string(j) + "-й набор данных" << endl;
       string path = absolute_input_path + "/" + to_string(j) + "/" + file_name;
       ifstream input_file(path);
-      if (input_file.is_open()){
+      if (input_file.is_open()) {
         string test_substr;
-        while (getline(input_file, test_substr)){
-          for(int k = 0; k < 10; k++){
+        while (getline(input_file, test_substr)) {
+          for (int k = 0; k < 10; k++) {
             const auto time_point_before = chrono::high_resolution_clock::now();
             suffixTree.hasSubstring(test_substr.substr(0, test_substr.length() - 1));
             const auto time_point_after = chrono::high_resolution_clock::now();
             const auto time_diff = time_point_after - time_point_before;
             const long time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
-            if (output_file.is_open()){
+            if (output_file.is_open()) {
               output_file << time_elapsed_ns << endl;
             }
           }
